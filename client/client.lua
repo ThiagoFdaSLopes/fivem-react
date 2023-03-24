b5d846616404134fe767d6b998df8f2d2bcac8f5
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-24 09:20:04 -0300

    feat: add modules

--- client/client.lua
+++ client/client.lua
@@ -1,3 +1,19 @@
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- VRP
+-----------------------------------------------------------------------------------------------------------------------------------------
+local Tunnel = module("vrp", "lib/Tunnel")
+local Proxy = module("vrp", "lib/Proxy")
+vRPC = Tunnel.getInterface("vRP")
+vRP = Proxy.getInterface("vRP")
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- CONNECTION
+-----------------------------------------------------------------------------------------------------------------------------------------
+cRP = {}
+Tunnel.bindInterface(GetCurrentResourceName(), cRP)
+
+vSERVER = Tunnel.getInterface(GetCurrentResourceName())
+
+
 local function toggleNuiFrame(shouldShow)
   SetNuiFocus(shouldShow, shouldShow)
   SendReactMessage('setVisible', shouldShow)
@@ -5,21 +21,19 @@ end
 
 RegisterCommand('show-nui', function()
   toggleNuiFrame(true)
-  debugPrint('Show NUI frame')
 end)
 
 RegisterNUICallback('hideFrame', function(_, cb)
   toggleNuiFrame(false)
-  debugPrint('Hide NUI frame')
   cb({})
 end)
 
 RegisterNUICallback('getClientData', function(data, cb)
-  debugPrint('Data sent by React', json.encode(data))
+	local Items = {}
+	local ped = PlayerPedId()
 
--- Lets send back client coords to the React frame for use
-  local curCoords = GetEntityCoords(PlayerPedId())
-
-  local retData <const> = { x = curCoords.x, y = curCoords.y, z = curCoords.z }
-  cb(retData)
+	local inventario,invPeso,invMaxpeso = vSERVER.requestInventory()
+	if inventario then
+		cb({ inventario = inventario, drop = Items, invPeso = invPeso, invMaxpeso = invMaxpeso })
+	end
 end)
