cc0e4c772fdcdc177cb84a8172c1584b72da0cfa
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-24 13:44:32 -0300

    feat: add global Styles

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { VisibilityProvider } from './providers/VisibilityProvider';
 import App from './components/App';
-import './index.css';
+import { ResetCss } from './theme/globalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <VisibilityProvider>
-      <App />
+      <ResetCss />
+        <App />
     </VisibilityProvider>
   </React.StrictMode>,
 );
