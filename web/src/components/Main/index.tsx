04925e43d8d653c4f89f0ee92b589bb41d63239a
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-24 09:21:22 -0300

    feat: Component Main

---
+++ web/src/components/Main/index.tsx
@@ -0,0 +1,12 @@
+import React, { Context, useContext, useState } from 'react'
+import { VisibilityCtx, VisibilityProviderValue } from '../../providers/VisibilityProvider'
+import Header from '../Header/Header'
+
+const Main: React.FC =() => {
+
+  return (
+    <Header />
+  )
+}
+
+export default Main
