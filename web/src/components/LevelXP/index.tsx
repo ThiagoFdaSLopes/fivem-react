3bb75066fc0d7ce8d62aba0197cb17bafec5a88d
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-24 18:03:00 -0300

    feat: New Progress Bar

---
+++ web/src/components/LevelXP/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as Progress from './levelxp'
+
+const LevelXP: React.FC = () => {
+  return (
+    <Progress.ProgressBar></Progress.ProgressBar>
+  )
+}
+
+export default LevelXP
