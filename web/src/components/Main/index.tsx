f1697296eb30afc216771d5dbd601cf4528f15e3
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-24 09:38:23 -0300

    feat: Main receive boxpass

--- web/src/components/Main/index.tsx
+++ web/src/components/Main/index.tsx
@@ -1,6 +1,7 @@
 import React, { Context, useContext, useState } from 'react'
 import { VisibilityCtx, VisibilityProviderValue } from '../../providers/VisibilityProvider'
 import Header from '../Header/Header'
+import Boxpass from '../BoxPass/Boxpass'
 import './main.css'
 
 const Main: React.FC =() => {
@@ -8,6 +9,7 @@ const Main: React.FC =() => {
   return (
     <main className='main'>
       <Header />
+      <Boxpass />
     </main>
   )
 }
