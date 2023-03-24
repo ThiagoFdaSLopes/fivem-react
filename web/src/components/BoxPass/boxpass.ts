0d5220a18a5fee5e73bf327e269ebfdd7567ae1c
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-24 13:44:41 -0300

    feat: Add BoxPass Styled

---
+++ web/src/components/BoxPass/boxpass.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  position: fixed;
+  top: 10rem;
+  left: 5rem;
+  width: 50%;
+  height: 45rem;
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  background-color: rgba(82, 5, 155, 0.8);
+`
