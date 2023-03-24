772879e89f190af00566a35a82909cd813cce8ea
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-24 09:27:13 -0300

    fix: add tag main

--- web/src/components/Main/index.tsx
+++ web/src/components/Main/index.tsx
@@ -1,11 +1,14 @@
 import React, { Context, useContext, useState } from 'react'
 import { VisibilityCtx, VisibilityProviderValue } from '../../providers/VisibilityProvider'
 import Header from '../Header/Header'
+import './main.css'
 
 const Main: React.FC =() => {
 
   return (
-    <Header />
+    <main className='main'>
+      <Header />
+    </main>
   )
 }
 
