bcf163099368bfcdfad1522a3141cee2e56c305c
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-25 11:31:54 -0300

    feat: Add Logica button Battle Pass

--- web/src/components/BoxPass/index.tsx
+++ web/src/components/BoxPass/index.tsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { Context, useContext } from 'react'
 import * as BoxPass from './boxpass'
 import HeaderBoxPass from '../HeaderBoxPass'
+import { VisibilityCtx, VisibilityProviderValue } from '../../providers/VisibilityProvider';
 import LevelXP from '../LevelXP';
 import Options from '../Options';
 
 const Boxpass: React.FC = () => {
+  const { sBattlePass } = useContext<VisibilityProviderValue>(VisibilityCtx as Context<VisibilityProviderValue>)
   return (
-    <BoxPass.Container>
+    <>
+    { !sBattlePass ? (
+      <BoxPass.Container>
       <HeaderBoxPass />
-      <LevelXP color={'purple'}/>
+      <LevelXP color={'black'}/>
       <Options />
     </BoxPass.Container>
-  )
+    ) : (
+      <div>Teste</div>
+    ) }
+    
+    </>
+  );
 }
 
 export default Boxpass
