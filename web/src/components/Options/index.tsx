f5487b0c87d400d18dcce4d6ecade1725059d953
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-25 11:32:02 -0300

    feat: OnClick Add

--- web/src/components/Options/index.tsx
+++ web/src/components/Options/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { Context, useContext } from 'react'
 import * as Op from './options'
 import OptionsButton from '../OptionsButtons'
-import backgroundOne from '../../imgs/battlepassButtonOne.jpg'
+import { VisibilityProviderValue, VisibilityCtx } from '../../providers/VisibilityProvider'
 
 const Options: React.FC = () => {
+  const { showBattlePass } = useContext<VisibilityProviderValue>(VisibilityCtx as Context<VisibilityProviderValue>)
   return (
     <Op.Box>
-      <OptionsButton backgroundColor='black' grid='1 / 1 / 3 / 2' name='Battle Pass'/>
+      <OptionsButton backgroundColor='black' grid='1 / 1 / 3 / 2' name='Battle Pass' onclick={showBattlePass}/>
       <OptionsButton backgroundColor='green' grid='1 / 2 / 2 / 3' name='Comum'/>
       <OptionsButton backgroundColor='red' grid='1 / 3 / 2 / 4' name='LENDARIO' />
       <OptionsButton backgroundColor='blue' grid='2 / 2 / 3 / 3' name='RARO'/>
