cefc142c3b2c6d4e4bcb1168c81632b47e3d44b0
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-25 09:58:43 -0300

    fix: Add options

--- web/src/components/BoxPass/index.tsx
+++ web/src/components/BoxPass/index.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import * as BoxPass from './boxpass'
 import HeaderBoxPass from '../HeaderBoxPass'
 import LevelXP from '../LevelXP';
+import Options from '../Options';
 
 const Boxpass: React.FC = () => {
   return (
     <BoxPass.Container>
       <HeaderBoxPass />
       <LevelXP color={'purple'}/>
+      <Options />
     </BoxPass.Container>
   )
 }
