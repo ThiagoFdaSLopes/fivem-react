7155cf3e850f1949a061094d7b8748ee72c8386e
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-25 10:48:12 -0300

    feat: Css tag p

--- web/src/components/OptionsButtons/optionsbuttons.ts
+++ web/src/components/OptionsButtons/optionsbuttons.ts
@@ -6,4 +6,33 @@ export const Button = styled.button<ButtonProps>`
   grid-area: ${(props) => `${props.grid}`};
   border: none;
   background-color: ${(props) => props.backgroundColor};
+  padding: 0;
+  margin: 0;
+
+  p {
+    font-style: bold;
+    font-weight: 900;
+    font-size: 52px;
+    padding: 0;
+    margin: 0;
+    text-align: center;
+    color: white;
+    text-transform: uppercase;
+  }
+
+  &:nth-child(n+2) {
+      font-size: 22px;
+  }
+
+  &:nth-child(1) {
+    p {
+      font-size: 52px;
+    }
+  }
+
+  &:nth-child(n+2) {
+    p {
+      font-size: 22px;
+    }
+  }
 `
