f30bd9e39a014f0a61bc6d81899073d10f9d88c9
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-25 11:32:19 -0300

    feat: Logica do onClick add no component

--- web/src/components/OptionsButtons/index.tsx
+++ web/src/components/OptionsButtons/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { Context, useContext } from 'react'
 import * as Bt from'./optionsbuttons'
+// import { VisibilityProviderValue, VisibilityCtx } from '../../providers/VisibilityProvider'
 
 export interface ButtonProps {
   name?: string
   backgroundColor?: string
   grid?: string
   img?: string
+  onclick?: () => void
 }
 
-const OptionsButton: React.FC<ButtonProps> = ({ name, backgroundColor, grid, img }) => {
+const OptionsButton: React.FC<ButtonProps> = ({ name, backgroundColor, grid, onclick }) => {
   return (
-    <Bt.Button backgroundColor={ backgroundColor } grid={grid}>
+    <Bt.Button backgroundColor={ backgroundColor } grid={grid} onClick={onclick}>
       <p>{name}</p>
     </Bt.Button>
   )
