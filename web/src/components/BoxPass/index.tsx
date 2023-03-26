7dd7b773027297c3d4356da007b52ef8577eaada
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-26 13:22:13 -0300

    feat: Container By Items

--- web/src/components/BoxPass/index.tsx
+++ web/src/components/BoxPass/index.tsx
@@ -4,6 +4,7 @@ import HeaderBoxPass from '../HeaderBoxPass'
 import { VisibilityCtx, VisibilityProviderValue } from '../../providers/VisibilityProvider';
 import LevelXP from '../LevelXP';
 import Options from '../Options';
+import ContainerSlick from '../ContainerSlick';
 
 const Boxpass: React.FC = () => {
   const { sBattlePass } = useContext<VisibilityProviderValue>(VisibilityCtx as Context<VisibilityProviderValue>)
@@ -16,7 +17,11 @@ const Boxpass: React.FC = () => {
       <Options />
     </BoxPass.Container>
     ) : (
-      <div>Teste</div>
+      <BoxPass.ContainerPass>
+        <HeaderBoxPass />
+        <LevelXP color='black'/>
+        <ContainerSlick />
+      </BoxPass.ContainerPass>
     ) }
     
     </>
