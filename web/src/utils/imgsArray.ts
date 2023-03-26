88ac17ebb7b739ffff3b1aef912c48bd9a22e059
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-26 14:35:12 -0300

    feat: Arrays imgs

--- web/src/utils/imgsArray.ts
+++ web/src/utils/imgsArray.ts
@@ -8,6 +8,18 @@ import amber from '../imgs/amber.png';
 import ak103 from '../imgs/ak103.png';
 import akcompact from '../imgs/akcompact.png';
 import alcohol from '../imgs/alcohol.png'
+import aluminum from '../imgs/aluminum.png';
+import cheese from '../imgs/cheese.png';
+import chandon from '../imgs/chandon.png';
+import c4 from '../imgs/c4.png';
+import atifx45 from '../imgs/atifx45.png';
+import amethyst from '../imgs/amethyst.png';
+import chip from '../imgs/chip.png';
+import card01 from '../imgs/card01.png';
+import card02 from '../imgs/card02.png';
+import card03 from '../imgs/card03.png';
+import card04 from '../imgs/card04.png';
+
 
 
 const arrayImgs: string[] = [
@@ -21,6 +33,17 @@ const arrayImgs: string[] = [
   ak103,
   akcompact,
   alcohol,
+  aluminum,
+  cheese,
+  chandon,
+  c4,
+  atifx45,
+  amethyst,
+  chip,
+  card01,
+  card02,
+  card03,
+  card04,
 ]
 
 export default arrayImgs;
