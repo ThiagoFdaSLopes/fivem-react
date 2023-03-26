e3294765f2880c6b9a3b038774f85410492198bf
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-26 14:53:29 -0300

    feat: teste do onClick

--- web/src/components/Items/index.tsx
+++ web/src/components/Items/index.tsx
@@ -4,11 +4,12 @@ import * as Box from './item'
 interface Item {
   index: number
   img?: string
+  onclick?: () => void
 }
 
-const Items: React.FC<Item> = ({ index, img }) => {
+const Items: React.FC<Item> = ({ index, img, onclick }) => {
   return (
-    <Box.Container>
+    <Box.Container onClick={onclick}>
       <Box.Index>{ index }</Box.Index>
       <img src={img} alt="Item" />
     </Box.Container>
