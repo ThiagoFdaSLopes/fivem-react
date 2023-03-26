0f3278cc9f6146ba3f87d80045ea31d7ccf189f6
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-26 14:34:56 -0300

    feat: Refactor Provider methods

--- web/src/components/Nav/index.tsx
+++ web/src/components/Nav/index.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { Context, useContext } from "react";
 import * as N from './nav'
 import { Button } from "../Button";
+import { VisibilityProviderValue, VisibilityCtx } from "../../providers/VisibilityProvider";
 
 const Nav: React.FC = () => {
+  const { handleToogle, showBattlePass } = useContext<VisibilityProviderValue>(VisibilityCtx as Context<VisibilityProviderValue>)
   return (
     <N.Nav>
-      <Button name='Play'/>
-      <Button name='Server Pass'/>
+      <Button name='Play' onclick={handleToogle}/>
+      <Button name='Server Pass' onclick={showBattlePass}/>
       <Button name='Complete'/>
       <Button name='Locker'/>
       <Button name='Item Shop'/>
