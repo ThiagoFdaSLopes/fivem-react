46d74f3362404a03e6b3b4af04f4f65d17c0e662
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-26 13:22:26 -0300

    feat: Container React Slick

---
+++ web/src/components/ContainerSlick/container.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ContainerBox = styled.div`
+  width: 60rem;
+  height: 17rem;
+  margin: auto;
+`
---
+++ web/src/components/ContainerSlick/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import * as Box from './container'
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import Items from '../Items';
+import arrayImgs from '../../utils/imgsArray';
+
+const ContainerSlick: React.FC = () => {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+  };
+
+  const newItems = arrayImgs.map((img, index) => (
+    <Items key={ index + 1 } img={ img } index={ index }/>
+  ))
+
+  return (
+    <Box.ContainerBox>
+
+
+      <Slider {...settings}>
+          { newItems }
+      </Slider>
+    </Box.ContainerBox>
+  )
+}
+
+export default ContainerSlick
