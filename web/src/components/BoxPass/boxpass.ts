92cf90f274ec0af33083b3afc18d3649a85caec2
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-26 13:22:03 -0300

    feat: new container Boxpass

--- web/src/components/BoxPass/boxpass.ts
+++ web/src/components/BoxPass/boxpass.ts
@@ -11,3 +11,15 @@ export const Container = styled.section`
   border-radius: 10px;
   background-color: rgba(82, 5, 155, 0.8);
 `
+
+export const ContainerPass = styled.section`
+  position: fixed;
+  top: 10rem;
+  left: 5rem;
+  width: 60%;
+  height: 45rem;
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  background-color: rgba(82, 5, 155, 0.8);
+`
