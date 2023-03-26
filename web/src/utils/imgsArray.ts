8102fe636a13d14885a8bc30522169c564db436f
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-26 13:22:47 -0300

    feat: Items array

---
+++ web/src/utils/imgsArray.ts
@@ -0,0 +1,26 @@
+import absolut from '../imgs/absolut.png';
+import acerola from '../imgs/acerola.png';
+import acerolajuice from '../imgs/acerolajuice.png';
+import acetone from '../imgs/acetone.png';
+import adrenaline from '../imgs/adrenaline.png';
+import ak74 from '../imgs/ak74.png';
+import amber from '../imgs/amber.png';
+import ak103 from '../imgs/ak103.png';
+import akcompact from '../imgs/akcompact.png';
+import alcohol from '../imgs/alcohol.png'
+
+
+const arrayImgs: string[] = [
+  absolut,
+  acerola,
+  acerolajuice,
+  acetone,
+  adrenaline,
+  ak74,
+  amber,
+  ak103,
+  akcompact,
+  alcohol,
+]
+
+export default arrayImgs;
