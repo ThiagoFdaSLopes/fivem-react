25e05ef5020cf4986e4e12051f6722e09e4ad168
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-27 07:26:43 -0300

    feat: getDatatable

--- server/server.lua
+++ server/server.lua
@@ -15,44 +15,10 @@ vCLIENT = Tunnel.getInterface(GetCurrentResourceName())
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- REQUESTINVENTORY
 -----------------------------------------------------------------------------------------------------------------------------------------
-function cRP.requestInventory()
+function cRP.getDataClient()
 	local source = source
 	local user_id = vRP.getUserId(source)
-	if user_id then
-		local myInventory = {}
-		local inventory = vRP.userInventory(user_id)
-		for k, v in pairs(inventory) do
-			if (parseInt(v["amount"]) <= 0 or itemBody(v["item"]) == nil) then
-				vRP.removeInventoryItem(user_id, v["item"], parseInt(v["amount"]), false)
-			else
-				v["amount"] = parseInt(v["amount"])
-				v["name"] = itemName(v["item"])
-				v["peso"] = itemWeight(v["item"])
-				v["index"] = itemIndex(v["item"])
-				v["max"] = itemMaxAmount(v["item"])
-				v["type"] = itemType(v["item"])
-				v["desc"] = itemDescription(v["item"])
-				v["key"] = v["item"]
-				v["slot"] = k
-
-				local splitName = splitString(v["item"], "-")
-				if splitName[2] ~= nil then
-					if itemDurability(v["item"]) then
-						v["durability"] = parseInt(os.time() - splitName[2])
-						v["days"] = itemDurability(v["item"])
-					else
-						v["durability"] = 0
-						v["days"] = 1
-					end
-				else
-					v["durability"] = 0
-					v["days"] = 1
-				end
-
-				myInventory[k] = v
-			end
-		end
-
-		return myInventory, vRP.inventoryWeight(user_id), vRP.getWeight(user_id)
-	end
+	local dataTable = vRP.userData(user_id,"Datatable")
+	
+	return dataTable
 end
