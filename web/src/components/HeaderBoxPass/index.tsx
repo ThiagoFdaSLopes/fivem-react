b58b530964be58d4baf758efa97fa0fd3f98d3b7
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-03-27 07:26:59 -0300

    feat: Button para pegar data

--- web/src/components/HeaderBoxPass/index.tsx
+++ web/src/components/HeaderBoxPass/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import * as Header from './HeaderBoxPass'
 import battlepassimg from '../../imgs/battlepass.png';
+import { useVisibility } from '../../providers/VisibilityProvider';
 
 const HeaderBoxPass: React.FC = () => {
+
+  const { visible, setData, dataPlayer } = useVisibility()
+
   return (
     <Header.HeaderBoxPassContainer>
       <Header.TitleDiv>
@@ -10,6 +14,7 @@ const HeaderBoxPass: React.FC = () => {
         <Header.TagsPDiv>
           <Header.PBattlePass>BATTLE PASS</Header.PBattlePass>
           <Header.SeasonPassP>SEASON LEVEL <span>1</span></Header.SeasonPassP>
+          <button onClick={setData}>Button</button>
         </Header.TagsPDiv>
       </Header.TitleDiv>
     </Header.HeaderBoxPassContainer>
