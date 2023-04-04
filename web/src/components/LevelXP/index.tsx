53fde1aab874b58047f9ede3fb2a08bc94335936
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-04-04 18:13:38 -0300

    feat: ProgressBar Provisorio

--- web/src/components/LevelXP/index.tsx
+++ web/src/components/LevelXP/index.tsx
@@ -3,12 +3,14 @@ import * as Progress from './levelxp'
 
 interface Props {
   color: string;
+  width: number;
 }
 
-
-const LevelXP: React.FC<Props> = ({ color }) => {
+const LevelXP: React.FC<Props> = ({ width }) => {
   return (
-    <Progress.ProgressBar color={color}/>
+    <Progress.ProgressBar>
+      <Progress.BarProgress color="green" width={ width }/>
+    </Progress.ProgressBar>
   )
 }
 
