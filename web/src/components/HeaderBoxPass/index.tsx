abf56502c9d698d6e19b3a084d02007ec8fa2400
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-04-04 18:13:23 -0300

    fix: Button

--- web/src/components/HeaderBoxPass/index.tsx
+++ web/src/components/HeaderBoxPass/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import * as Header from './HeaderBoxPass'
 import battlepassimg from '../../imgs/battlepass.png';
-import { useVisibility } from '../../providers/VisibilityProvider';
 
 const HeaderBoxPass: React.FC = () => {
-
-  const { visible, setData, dataPlayer } = useVisibility()
 
   return (
     <Header.HeaderBoxPassContainer>
@@ -14,7 +11,6 @@ const HeaderBoxPass: React.FC = () => {
         <Header.TagsPDiv>
           <Header.PBattlePass>BATTLE PASS</Header.PBattlePass>
           <Header.SeasonPassP>SEASON LEVEL <span>1</span></Header.SeasonPassP>
-          <button onClick={setData}>Button</button>
         </Header.TagsPDiv>
       </Header.TitleDiv>
     </Header.HeaderBoxPassContainer>
