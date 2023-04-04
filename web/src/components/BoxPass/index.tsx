f4b6cd96fd1ba5883d2296879362f53869a5e886
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-04-04 18:13:15 -0300

    feat: GetDataPlayer

--- web/src/components/BoxPass/index.tsx
+++ web/src/components/BoxPass/index.tsx
@@ -1,25 +1,26 @@
-import React, { Context, useContext } from 'react'
+import React, { useEffect, useState } from 'react'
 import * as BoxPass from './boxpass'
 import HeaderBoxPass from '../HeaderBoxPass'
-import { VisibilityCtx, VisibilityProviderValue } from '../../providers/VisibilityProvider';
+import { useVisibility } from '../../providers/VisibilityProvider';
 import LevelXP from '../LevelXP';
 import Options from '../Options';
 import ContainerSlick from '../ContainerSlick';
 
 const Boxpass: React.FC = () => {
-  const { sBattlePass } = useContext<VisibilityProviderValue>(VisibilityCtx as Context<VisibilityProviderValue>)
+  const { sBattlePass, dataPlayer } = useVisibility();
+  const exp = (dataPlayer.experience/ 100) * 100;
   return (
     <>
     { !sBattlePass ? (
       <BoxPass.Container>
       <HeaderBoxPass />
-      <LevelXP color={'black'}/>
+      <LevelXP color='black' width={exp}/>
       <Options />
     </BoxPass.Container>
     ) : (
       <BoxPass.ContainerPass>
         <HeaderBoxPass />
-        <LevelXP color='black'/>
+        <LevelXP color='black' width={exp}/>
         <ContainerSlick />
       </BoxPass.ContainerPass>
     ) }
