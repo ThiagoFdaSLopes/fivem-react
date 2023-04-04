30a915aaf7d3bd075172d23afbb4d18d7cf62dc0
TornadoZer0 <64657910+TornadoZer0@users.noreply.github.com>
2023-04-04 18:13:49 -0300

    fix: Styled Component

--- web/src/components/LevelXP/levelxp.ts
+++ web/src/components/LevelXP/levelxp.ts
@@ -1,11 +1,21 @@
 import styled from 'styled-components';
 
 interface LevelUp {
-  color: string;
+  width: number;
 }
 
-export const ProgressBar = styled.div<LevelUp>`
+export const ProgressBar = styled.div`
+  background-color: black;
+  width: 100%;
+  height: 20px;
+  line-height:34px;
+  text-align: center;
+  font-size: 24px;
+`
+
+export const BarProgress = styled.div<LevelUp>`
   background-color: ${(props) => props.color};
+  width: ${(props) => `${props.width}%`};
   height: 20px;
   line-height:34px;
   text-align: center;
